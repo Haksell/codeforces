30f6737b4b1ebe234eae622e5ad0ecb77ff3ede2
Axel Brisse <axel.brisse@gmail.com>
2024-09-20 13:37:18 +0200

    1087

---
+++ 1913A.py
@@ -0,0 +1,13 @@
+import re
+
+for _ in range(int(input())):
+    ab = input()
+    m = re.fullmatch(r"([1-9]0*)([1-9]\d*)", ab)
+    if not m:
+        print(-1)
+        continue
+    a, b = map(int, m.groups())
+    if a >= b:
+        print(-1)
+    else:
+        print(a, b)
---
+++ 1918B.py
@@ -0,0 +1,9 @@
+for _ in range(int(input())):
+    n = int(input())
+    a = list(map(int, input().split()))
+    b = list(map(int, input().split()))
+    print(*range(1, n + 1))
+    idx = [-1] * n
+    for i, ai in enumerate(a):
+        idx[ai - 1] = i
+    print(*[b[i] for i in idx])
