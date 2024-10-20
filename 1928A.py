aa1c6cccbc6448a5a401048666203373f938c03c
Axel Brisse <axel.brisse@gmail.com>
2024-10-20 23:51:42 +0200

    .

--- 1928A.py
+++ 1928A.py
@@ -1,3 +1,5 @@
 for _ in range(int(input())):
     a, b = map(int, input().split())
-    print("No" if a & b & 1 or a == b << 1 or a << a == b else "Yes")
+    if a > b:
+        a, b = b, a
+    print("NO" if a & 1 and (b & 1 or a << 1 == b) else "YES")
