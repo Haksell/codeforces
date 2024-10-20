83d6692c99135d4809a541e679c563bc7876398b
Axel Brisse <axel.brisse@gmail.com>
2024-10-20 21:43:08 +0200

    .

---
+++ 2009C.py
@@ -0,0 +1,7 @@
+def int_ceil(n, k):
+    return (n + k - 1) // k
+
+
+for _ in range(int(input())):
+    x, y, k = map(int, input().split())
+    print(max(2 * int_ceil(x, k) - 1, 2 * int_ceil(y, k)))
