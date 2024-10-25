ad5e58a9df94c7bfaf2295e99ff640ab155a11d1
Axel Brisse <axel.brisse@gmail.com>
2024-10-25 03:28:34 +0200

    .

--- 0414B.py
+++ 0414B.py
@@ -1,12 +1,12 @@
 MOD = 1_000_000_007
+
 n, k = map(int, input().split())
 divisors = sorted({n // i for i in range(1, n + 1)})
 div_idx = [len(divisors)] * (n + 1)
 for i, d in enumerate(divisors):
     div_idx[d] = i
 
-dp = [[int(i == 0)] * len(divisors) for i in range(k + 1)]
-for kk in range(1, k + 1):
-    for i, d in enumerate(divisors):
-        dp[kk][i] = sum(dp[kk - 1][div_idx[d // i]] for i in range(1, d + 1)) % MOD
-print(dp[-1][-1])
+dp = [1] * len(divisors)
+for _ in range(k):
+    dp = [sum(dp[div_idx[d // i]] for i in range(1, d + 1)) % MOD for d in divisors]
+print(dp[-1])
