8096bc2869e271bc45106824d144fd9fc8dd30b3
Axel Brisse <axel.brisse@gmail.com>
2024-10-27 12:42:44 +0100

    .

---
+++ 2027B.py
@@ -0,0 +1,12 @@
+for _ in range(int(input())):
+    n = int(input())
+    a = list(map(int, input().split()))
+    res = n
+    i = 0
+    while i < res:
+        m = max(a)
+        mi = a.index(m)
+        res = min(res, i + mi)
+        a.remove(m)
+        i += 1
+    print(res)
