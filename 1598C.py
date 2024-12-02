8e6fd46c8e7bbd888bd89273f4578e31ded7e9d6
Axel Brisse <axel.brisse@gmail.com>
2024-12-02 08:03:36 +0100

    handshakes

---
+++ 1598C.py
@@ -0,0 +1,36 @@
+# ruff: noqa: E731, E741
+from collections import Counter
+import sys
+
+read = sys.stdin.readline
+input = lambda: read().strip()
+ir = lambda: int(read())
+rir = lambda: range(int(read()))
+mir = lambda: map(int, read().split())
+lmir = lambda: list(map(int, read().split()))
+
+
+def handshake(n):
+    return n * (n - 1) >> 1
+
+
+def main():
+    for _ in rir():
+        n = ir()
+        a = lmir()
+        s = sum(a)
+        d, m = divmod(s, n)
+        if m == 0:
+            target = d << 1
+        elif 2 * m == n:
+            target = d << 1 | 1
+        else:
+            print(0)
+            continue
+        c = Counter(a)
+        diff = sum(v * c[target - k] for k, v in c.items() if 2 * k != target) >> 1
+        print(diff + handshake(c[d]) if m == 0 else diff)
+
+
+if __name__ == "__main__":
+    main()
