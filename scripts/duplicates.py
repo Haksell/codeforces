bbcd2fbef873be48dec8c3870ad31e30dc277fce
Haksell <axel.brisse@gmail.com>
2024-12-04 20:36:02 +0100

    show symlinks in cyan

--- scripts/duplicates.py
+++ scripts/duplicates.py
@@ -2,6 +2,10 @@ from collections import defaultdict
 import os
 
 
+def bold_cyan(text):
+    return f"\033[1;36m{text}\033[0m"
+
+
 def main():
     contents = defaultdict(list)
     for file in sorted(os.listdir()):
@@ -9,9 +13,13 @@ def main():
             continue
         content = open(file).read()
         contents[content].append(file)
-    for v in contents.values():
-        if len(v) >= 2:
-            print(" == ".join(v))
+    for files in contents.values():
+        if len(files) >= 2:
+            print(
+                " == ".join(
+                    bold_cyan(file) if os.path.islink(file) else file for file in files
+                )
+            )
 
 
 if __name__ == "__main__":
