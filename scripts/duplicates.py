49967eb6fe8ce71430cbeac542f37217e81530b7
Axel Brisse <axel.brisse@gmail.com>
2024-12-12 06:47:53 +0100

    better duplicates.py

--- scripts/duplicates.py
+++ scripts/duplicates.py
@@ -2,6 +2,9 @@ from collections import defaultdict
 import os
 
 
+DIR = "problems"
+
+
 def title(s):
     print(f" {s} ".center(40, "="))
 
@@ -10,33 +13,44 @@ def bold_cyan(text):
     return f"\033[1;36m{text}\033[0m"
 
 
+def show_files(files):
+    print(
+        " == ".join(
+            bold_cyan(f) if os.path.islink(os.path.join(DIR, f)) else f for f in files
+        )
+    )
+
+
 def show_standalone_duplicates(standalone):
     title("STANDALONE DUPLICATES")
     for files in standalone.values():
         if len(files) >= 2:
-            print(
-                " == ".join(
-                    bold_cyan(file) if os.path.islink(file) else file for file in files
-                )
-            )
+            show_files(files)
 
 
+# TODO: handle >2 subtasks (YAGNI though)
 def show_subtasks(subtasks):
-    title("SUBTASKS")
+    DIFFERENT = {"2039C"}
+    title("SAME SUBTASKS")
     for k, v in sorted(subtasks.items()):
-        print(k, v.keys())
+        if k not in DIFFERENT:
+            show_files(v)
+    title("DIFFERENT SUBTASKS")
+    for k, v in sorted(subtasks.items()):
+        if k in DIFFERENT:
+            show_files(v)
 
 
 def main():
     standalone = defaultdict(list)
-    subtasks = defaultdict(dict)
-    for file in sorted(os.listdir()):
+    subtasks = defaultdict(list)
+    for file in sorted(os.listdir(DIR)):
         if not file.endswith(".py"):
             continue
-        content = open(file).read()
         if file[-4].isdigit():
-            subtasks[file[:-4]][file[-4]] = content
+            subtasks[file[:-4]].append(file)
         else:
+            content = open(os.path.join(DIR, file)).read()
             standalone[content].append(file)
     show_standalone_duplicates(standalone)
     show_subtasks(subtasks)
