7c43383a7ea7634af278023a211e783f4ab9769b
Haksell <axel.brisse@gmail.com>
2024-12-20 21:43:22 +0100

    simple 2049B

--- problems/2049B.py
+++ problems/2049B.py
@@ -9,21 +9,11 @@ mir = lambda: map(int, read().split())
 lmir = lambda: list(map(int, read().split()))
 
 
-def solve(n, s):
-    pi = s.find("p")
-    si = s.rfind("s")
-    if pi == -1 or si == -1:
-        return True
-    if pi < si:
-        return False
-    return si == 0 or pi == n - 1
-
-
 def main():
     for _ in rir():
-        n = ir()
+        read()
         s = input()
-        print("YES" if solve(n, s) else "NO")
+        print("NO" if "s" in s[1:] and "p" in s[:-1] else "YES")
 
 
 if __name__ == "__main__":
