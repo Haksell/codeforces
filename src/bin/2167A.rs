f6013b312351c77803ef72beb0c2412280cb2f3c
Haksell <axel.brisse@gmail.com>
2026-06-25 08:48:35 +0200

    first problem solved in Rust

---
+++ src/bin/2167A.rs
@@ -0,0 +1,8 @@
+use codeforces::{read, yes_no};
+
+fn main() {
+    for _ in 0..read!(u32) {
+        let (a, b, c, d) = read!(u32, u32, u32, u32);
+        yes_no(a == b && a == c && a == d);
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -2,17 +2,13 @@ mod read_macro;
 
 pub fn input() -> String {
     let mut s = String::new();
-    std::io::stdin()
-        .read_line(&mut s)
-        .expect("Failed to read line");
+    std::io::stdin().read_line(&mut s).expect("Failed to read line");
     s.truncate(s.trim_end().len());
     s
 }
 
 pub fn skip_line() {
-    std::io::stdin()
-        .read_line(&mut String::new())
-        .expect("Failed to skip a line");
+    std::io::stdin().read_line(&mut String::new()).expect("Failed to skip a line");
 }
 
 pub fn read_vec<T>() -> Vec<T>
@@ -20,8 +16,9 @@ where
     T: core::fmt::Debug + core::str::FromStr,
     T::Err: core::fmt::Debug,
 {
-    input()
-        .split_whitespace()
-        .map(|x| x.parse::<T>().unwrap())
-        .collect()
+    input().split_whitespace().map(|x| x.parse::<T>().unwrap()).collect()
+}
+
+pub fn yes_no(b: bool) {
+    println!("{}", if b { "YES" } else { "NO" });
 }
